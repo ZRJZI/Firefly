ce969811d3d146e51499b3be71ff576a1433f752
ZRJZI <125759969+ZRJZI@users.noreply.github.com>
2026-08-29 03:40:18 +0800

    Update siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -110,8 +110,8 @@ export const siteConfig: SiteConfig = {
 		// 使用 Astro 图标库时不需要设置 valueDark，图标会自动跟随主题亮暗色切换
 		logo: {
 			type: "image",
-			value: "assets/images/logo/sakura.png",
-			valueDark: "assets/images/logo/sakura.png",
+			value: "/assets/images/logo/sakura.png",
+			valueDark: "/assets/images/logo/sakura.png",
 			alt: "🍀",
 		},
 		// 导航栏标题
