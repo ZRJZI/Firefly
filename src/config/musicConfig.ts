ffb478f6979041a5d2eb6ea4352c9a187c358846
ZRJZI <125759969+ZRJZI@users.noreply.github.com>
2026-08-28 18:23:29 +0800

    Update musicConfig.ts

--- src/config/musicConfig.ts
+++ src/config/musicConfig.ts
@@ -50,7 +50,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 			{
 				name: "使一颗心免于哀伤",
 				artist: "知更鸟 / HOYO-MiX / Chevy",
-				url: "/assets/music/使一颗心免于哀伤-哼唱.mp3",
+				url: "/assets/music/tayori - 夢遊.flac",
 				cover: "/assets/music/cover/109951169585655912.webp",
 				lrc: "",
 			},
