f7927c351fd5ecc159d989439d0b6520186205fc
ZRJZI <125759969+ZRJZI@users.noreply.github.com>
2026-08-29 00:27:30 +0800

    Update profileConfig.ts

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -9,10 +9,10 @@ export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.avif",
 
 	// 名字
-	name: "Firefly",
+	name: "Mitsuka",
 
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "灌注Mitsuka谢谢喵.",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
@@ -24,19 +24,19 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "qq",
 			icon: "fa7-brands:qq",
-			url: "https://qm.qq.com/q/ZGsFa8qX2G",
+			url: "https://qm.qq.com/q/K8yDJ7frCa",
 			showName: false,
 		},
 		{
 			name: "GitHub",
 			icon: "fa7-brands:github",
-			url: "https://github.com/CuteLeaf",
+			url: "https://github.com/ZRJZI",
 			showName: false,
 		},
 		{
 			name: "Email",
 			icon: "fa7-solid:envelope",
-			url: "mailto:xiaye@msn.com",
+			url: "zrjzi@qq.com",
 			showName: false,
 		},
 		{
