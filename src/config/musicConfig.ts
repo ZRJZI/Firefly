9b3d250d5b12f24cef7193b27ec83f1a243e0931
ZRJZI <125759969+ZRJZI@users.noreply.github.com>
2026-08-28 23:43:44 +0800

    Update musicConfig.ts

--- src/config/musicConfig.ts
+++ src/config/musicConfig.ts
@@ -42,16 +42,16 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 
 	// 本地音乐配置（当 mode 为 'local' 时使用）
 	// 1. 支持传入歌词文件的路径
-	// lrc: "/assets/music/lrc/使一颗心免于哀伤-哼唱.lrc",
+	// lrc: "/assets/music/lrc/夢遊.lrc",
 	// 2. 或者直接填入歌词字符串内容
 	// lrc: "[00:00.00]歌词内容...",
 	local: {
 		playlist: [
 			{
-				name: "使一颗心免于哀伤",
+				name: "夢遊",
 				artist: "知更鸟 / HOYO-MiX / Chevy",
 				url: "/assets/music/tayori - 夢遊.flac",
-				cover: "/assets/music/cover/109951169585655912.webp",
+				cover: "/assets/music/cover/memento-600.webp",
 				lrc: "",
 			},
 		],
